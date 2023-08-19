c560b9a6d54543dc100d5ee9196e6bbe20229a8f
kksahay <kunal.sahay4@gmail.com>
2023-08-19 17:13:38 +0530

    fix: controller cleanup

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 import pg from 'pg'
 const { Pool } = pg
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export const pool = new Pool({
-    user: "postgres",
-    database: "leetcode",
-    password: "1202",
-    host: "localhost",
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT
 });
