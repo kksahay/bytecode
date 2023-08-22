b5293989e29a2e53d85504d855f42e8d29a80c2e
kksahay <kunal.sahay4@gmail.com>
2023-08-23 04:13:03 +0530

    Stop tracking .env file

--- controllers/problemsetController.js
+++ controllers/problemsetController.js
@@ -26,15 +26,15 @@ export const taskController = async (req, res) => {
 
 export const createProblemController = async (req, res) => {
     try {
-        const { name, description, difficulty, constraint } = req.body
+        const { name, description, difficulty, input, output, constraint, sample_input, sample_output } = req.body
         if (!name) { return res.send("Problem name is not given") }
         if (!description) { return res.send("Problem Description is missing") }
         if (!difficulty) { return res.send("Problem difficulty is not given") }
         if (!constraint) { return res.send("Problem constraint is not given") }
         console.log(constraint)
         const problem = await pool.query(
-            "INSERT INTO problemset (_name, _description, _difficulty, _constraint) VALUES ($1, $2, $3, $4)",
-            [name, description, difficulty, constraint]
+            "INSERT INTO problemset (_name, _description, _difficulty, _input, _output, _constraint, _sample_input, _sample_output) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+            [name, description, difficulty, input, output, constraint, sample_input, sample_output]
         )
         res.status(200).send({
             success: true
